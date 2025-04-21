8459cbce4ed90989c40bf4d7a31513340bfd24ed
Barathram46 <barathram764@gmail.com>
2025-04-21 20:57:00 +0530

    commit

--- backend/routes/restaurantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const restaurantController = require("../controller/restaurantController"); // ✅ Correct import
-
-router.get("/", restaurantController.getRestaurants);
-
-
-module.exports = router;
